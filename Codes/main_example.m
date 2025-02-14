fd0d54e917d45d96532930f7c095fc3926e4b325
Giovanni Traetta <149532258+innavoig23@users.noreply.github.com>
2025-02-14 17:11:32 +0100

    Update main_example.m

--- Codes/main_example.m
+++ Codes/main_example.m
@@ -6,70 +6,45 @@ clc
 load './exMUPulses.mat';
 fsamp = 2048; % the signals of the example was acquired ad 2048 Hz
 sigLen = 30 * fsamp; % the duration of the acquisition was 30 s
-numMUs = numel(MUPulses);
+numMUs = numel(MUPulses); % number of identified MUs (here I extracted only 2 MUs for simplicity)
 
 %% Compute IDR and smooth it
+% IDR
 IDR = cellfun(@(x) fsamp./diff(x), MUPulses, 'UniformOutput',false);
 timeInst = cellfun(@(x) x(2:end)/fsamp, MUPulses, 'UniformOutput',false);
 
+% Smoothed IDR
 smoothedIDR = smoothFiringRateMU(MUPulses, fsamp, sigLen);
 timeSmoothed = (0:length(smoothedIDR(1,:))-1) / fsamp;
 
 %% Plot
-matBlue = [0 0.4470 0.7410];
-matRed = [0.8500 0.3250 0.0980];
+matBlue = [0 0.4470 0.7410]; % blue for plot
+matRed = [0.8500 0.3250 0.0980]; % red for plot
 
 for mu = 1:numMUs
 
+    % Figure Creation
     figure('Name', sprintf("MU %d - Firing Rate", mu), 'WindowState', 'maximized');
     
+    % IDR and Smoothed IDR Plot
     yyaxis left
     scatter(timeInst{mu}, IDR{mu}, 'LineWidth', 1, 'MarkerEdgeColor', matBlue)
     hold on;
-    plot(timeSmoothed, smoothedIDR(mu, :), 'LineWidth', 2, 'Color', matBlue*1.1, 'LineStyle', '-'); % Forza linea continua
+    plot(timeSmoothed, smoothedIDR(mu, :), 'LineWidth', 2, 'Color', matBlue*1.1, 'LineStyle', '-');
     ylabel('Discharge Rate (pps)', 'FontWeight', 'bold')
     
+    % Force Plot
     yyaxis right
-    plot(timeSmoothed, forceSig*100, 'LineWidth', 2, 'Color', matRed, 'LineStyle', '-'); % Assicura linea continua
+    plot(timeSmoothed, forceSig*100, 'LineWidth', 2, 'Color', matRed, 'LineStyle', '-');
     ylabel('Force (% MVC)', 'FontWeight', 'bold')
     ylim([0 25])
     
-    xlim([timeSmoothed(1), timeSmoothed(end)])
+    % Plot Settings
+    xlim([timeSmoothed(1), timeSmoothed(end)+1/fsamp])
     xlabel('Time (s)', 'FontWeight', 'bold')
 
     set(gca, 'FontSize', 24);
     title(sprintf("MU %d - Firing Rate", mu), 'FontWeight', 'bold', 'FontSize',26)
-
-    legend('IDR', 'Smoothed IDR')
-
-end
-
-%% Plot
-matBlue = [0 0.4470 0.7410];
-matRed = [0.8500 0.3250 0.0980];
-
-figure('Name','MUs Firing Rate', 'WindowState','maximized');
-sgtitle('MUs Firing Rate', 'FontWeight', 'bold', 'FontSize',26)
-
-for mu = 1:numMUs
-
-    subplot(2,1,mu)
-
-    yyaxis left
-    scatter(timeInst{mu}, IDR{mu}, 'LineWidth', 1, 'MarkerEdgeColor', matBlue)
-    hold on;
-    plot(timeSmoothed, smoothedIDR(mu, :), 'LineWidth', 2, 'Color', matBlue*1.1, 'LineStyle', '-'); % Forza linea continua
-    ylabel('Discharge Rate (pps)', 'FontWeight', 'bold')
-    
-    yyaxis right
-    plot(timeSmoothed, forceSig*100, 'LineWidth', 2, 'Color', matRed, 'LineStyle', '-'); % Assicura linea continua
-    ylabel('Force (% MVC)', 'FontWeight', 'bold')
-    
-    xlim([timeSmoothed(1), timeSmoothed(end)])
-    xlabel('Time (s)', 'FontWeight', 'bold')
-
-    set(gca, 'FontSize', 24);
-    title(sprintf("MU %d", mu), 'FontWeight', 'bold', 'FontSize',26)
 
     legend('IDR', 'Smoothed IDR')
 
